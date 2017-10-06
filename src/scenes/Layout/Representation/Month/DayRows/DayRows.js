2e6eb38988798fc6fcdf8364111e49504d9fb0f5
Christos Zonios <czonios@gmail.com>
2017-10-06 10:56:20 +0300

    MODIFY tidy up comments

--- src/scenes/Layout/Representation/Month/DayRows/DayRows.js
+++ src/scenes/Layout/Representation/Month/DayRows/DayRows.js
@@ -8,28 +8,34 @@ const DayRows = ({ displayMonth, displayYear, dateService }) => (
 
 );
 
-
+/**
+ * For display on the month grid, if the first day of the month isn't sunday, null's need to be added
+ * onto the beggining of the array in order to adjust the display to the right
+ * EG if a month starts on wednesday, we want 3 null's padded onto the front of the days array 
+ * in order to adjust for sun through tue
+ */
 function padDaysInMonthArr(year, month, dateService) {
-  //For display on the month grid, if the first day of the month isn't sunday, null's need to be added
-  //onto the beggining of the array in order to adjust the display to the right
-  //EG if a month starts on wednesday, we want 3 null's padded onto the front of the days array 
-  //in order to adjust for sun through tue
+  
   const daysArr = dateService.getDaysInMonth(year, month);
   const dayCount = dateService.getDaysCountInMonth(year, month);
   const initialPaddingNeeded = daysArr[0];
   return Array.from({ length: initialPaddingNeeded }, () => null)
     .concat(Array.from({ length: dayCount }, (_, i) => i + 1));
 }
+
+//Transform the 1d padded array into a 2d array (5 x 7) representing the weeks of the month
 function splitDaysIntoWeeks(paddedArr) {
-  //Transform the 1d padded array into a 2d array (5 x 7) representing the weeks of the month
+  
   const chunckArr = [];
   for (let i = 0; i < 36; i += 7) {
     chunckArr.push(paddedArr.slice(i, i + 7));
   }
   return chunckArr;
 }
+
+//Consume the 2d array representing the weeks, and generate  5-6 Grid.Rows with the days as columns 
 function generateRows(chunkedArr) {
-  //Consume the 2d array representing the weeks, and generate  5-6 Grid.Rows with the days as columns 
+  
   return chunkedArr.map((subArr, i) => {
     return <Grid.Row key={i}>
       {subArr.map((day, j) => {
