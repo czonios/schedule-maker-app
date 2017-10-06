7a43822f2f3cb938f6f3452abe334bbd0e96ab49
Christos Zonios <czonios@gmail.com>
2017-10-06 16:09:50 +0300

    MODIFY UI for better UX

--- src/scenes/Layout/Representation/Month/MonthHeader/MonthHeader.js
+++ src/scenes/Layout/Representation/Month/MonthHeader/MonthHeader.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import { Button, Header } from 'semantic-ui-react';
+import { Button, Header, Image } from 'semantic-ui-react';
 import './monthHeader.css';
 
 const MonthHeader = (
   { displayMonth, incrementDisplayMonth, decrementDisplayMonth, displayYear, dateService }
 ) => (
     <div className="centered">
-      <Button compact onClick={decrementDisplayMonth}>
-        <img src={require('./img/arrow-left.svg')} alt="<" />
+      <Button basic compact onClick={decrementDisplayMonth}>
+        <Image className="arrow" size="mini" src={require('./img/arrow-left.svg')} alt="<" />
       </Button>
 
-      <Header size="small" className="month-year">
+      <Header size="small" color="white" className="month-year">
         {dateService.monthNamesLong[displayMonth]}
         <span>&#32;</span>
         {displayYear}
-        <span>&#32;</span>
       </Header>
 
-      <Button compact onClick={incrementDisplayMonth}>
-        <img src={require('./img/arrow-right.svg')} alt=">" />
+      <Button basic compact onClick={incrementDisplayMonth}>
+        <Image className="arrow" size="mini" src={require('./img/arrow-right.svg')} alt=">" />
       </Button>
     </div>
   )
