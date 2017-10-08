db656c54dfe4fb4bca33e559fb64bcdd976d81e4
Christos Zonios <czonios@gmail.com>
2017-10-08 20:50:31 +0300

    ADD modal in editevent, not working properly yet

--- src/scenes/Layout/components/Representation/components/Week/components/DayOfWeek/DayOfWeek.js
+++ src/scenes/Layout/components/Representation/components/Week/components/DayOfWeek/DayOfWeek.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import { Header } from 'semantic-ui-react';
-//import dateService from '../../../../../../services/dates/dateService';
+import './dayOfWeek.css';
+import EditEvent from '../../../../services/EditEvent';
 
-const DayOfWeek = (props) => (
-    <div>
-        <Header size="tiny">title for {props.time}</Header>
-        <p>description</p>
-    </div>
-);
+class DayOfWeek extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            showModal: false
+        };
+        this.setModal = this.setModal.bind(this);
+    }
+
+    setModal(event) {
+        if (this.state.showModal) {
+            this.setState({
+                showModal: false
+            });
+        }
+        else {
+            this.setState({
+                showModal: true
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div className="clickable" onClick={this.setModal.bind(this)}>
+                <Header size="tiny">title for {this.props.time}</Header>
+                <p>description</p>
+                <EditEvent show={this.state.showModal} />
+            </div>
+        )
+    }
+}
 
 export default DayOfWeek;
--- src/scenes/Layout/components/Representation/services/EditEvent.js
+++ src/scenes/Layout/components/Representation/services/EditEvent.js
@@ -4,37 +4,41 @@ import { Card, Modal, Button, Icon, Header } from 'semantic-ui-react';
 
 const EditEvent = (props) => (
 
-            {/* modal */}
-            <Modal trigger={<Button>View</Button>} closeIcon>
-                <Header icon='archive' content='View Event' />
+  <div>
+    {/* modal */}
+    <Modal trigger={props.show} closeIcon>
+      <Header icon='archive' content='View Event' />
 
-                {/* Show card */}
-                <Card centered >
-                <Card.Content extra className="event-start">
-                  {event.time.start}
-                </Card.Content>
-                <Card.Content>
-                  <Card.Header>
-                    {event.title}
-                  </Card.Header>
-                  <Card.Description>
-                    {event.description}
-                  </Card.Description>
-                </Card.Content>
-                <Card.Content extra className="event-end">
-                  {event.time.end}
-                </Card.Content>
-              </Card>
+      {/* Show card */}
+      <Card centered >
+        <Card.Content extra className="event-start">
+          {/* {event.time.start} */}
+        </Card.Content>
+        <Card.Content>
+          <Card.Header>
+            {/* {event.title} */}
+          </Card.Header>
+          <Card.Description>
+            {/* {event.description} */}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra className="event-end">
+          {/* {event.time.end} */}
+        </Card.Content>
+      </Card>
 
-                <Modal.Actions>
-                    <Button color='red'>
-                        <Icon name='remove' /> No
+      <Modal.Actions>
+        <Button color='red'>
+          <Icon name='remove' /> No
                     </Button>
-                    <Button color='green'>
-                        <Icon name='checkmark' /> Yes
-                    </Button>
-                </Modal.Actions>
-            </Modal>   
+        <Button color='green'>
+          <Icon name='checkmark' /> Yes
+        </Button>
+      </Modal.Actions>
+    </Modal>
+  </div>
 
 );
+
+export default EditEvent;
 
