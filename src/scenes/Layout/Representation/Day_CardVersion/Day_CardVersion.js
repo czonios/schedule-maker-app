42315f1d195b4b5ac9b4b75b1500cab715ccb980
paspheeris <pa.spheeris@gmail.com>
2017-10-07 19:35:10 -0700

    ADD Day_CardVersion working in basic form and set to render from Representation in place of the previous Day card

--- src/scenes/Layout/Representation/Day_CardVersion/Day_CardVersion.js
+++ src/scenes/Layout/Representation/Day_CardVersion/Day_CardVersion.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types'
 import timeService from '../../../../services/times/timeService';
 import { Card } from 'semantic-ui-react';
+import './Day_CardVersion.css';
 
 const propTypes = {
   events: PropTypes.array.isRequired
@@ -13,10 +14,21 @@ const Day_CardVersion = ({ events }) => (
   <div>
     {console.log(timeService.sortEventsByTimeMutable(events))}
     {timeService.sortEventsByTimeMutable(events).map((event, i) => (
-      <Card key={i}>
+      <Card key={i} centered >
         <Card.Header>
-          {event.title}
+          {event.time.start}
         </Card.Header>
+        <Card.Content>
+          <Card.Header>
+            {event.title}
+          </Card.Header>
+          <Card.Description>
+            {event.description}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra className="red-background">
+          {event.time.end}
+        </Card.Content>
       </Card>
     ))}
   </div>
