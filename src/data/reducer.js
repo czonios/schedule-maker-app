a9e51e13a5ac4616bad32cc2a82aa26bf91321fe
paspheeris <pa.spheeris@gmail.com>
2017-10-09 09:54:06 -0700

    ADDed a new slice to redux store UI, with field dayOfWeekAs0, which defaults to monday

---
+++ src/data/reducer.js
@@ -0,0 +1,12 @@
+const initState = {
+  dayOfWeekAs0: 'monday',
+};
+
+const UI = (state = initState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default UI;
--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import test_scene from './scenes/Test_Scene/reducer.js';
 import layout from './scenes/Layout/reducer.js';
+import UI from './data/reducer'
 
-
-const rootReducer = combineReducers({ layout, test_scene });
+const rootReducer = combineReducers({ layout, test_scene, UI });
 export default rootReducer;
